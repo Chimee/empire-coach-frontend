5c97d9389847797c8d93eb151b0ebdb89eebccc1
Tarun123-kittu <deepak.chauhan@ultivic.com>
2026-01-01 17:18:46 +0530

    changes

--- src/features/driverScreens/CompleteDelivery.jsx
+++ src/features/driverScreens/CompleteDelivery.jsx
@@ -1,25 +1,50 @@
+import React from 'react';
+import './style.css';
+import Button from '../../components/shared/buttons/button';
+import tripSvgImage from '../../images/tripEnded.svg';
+import { Image } from 'react-bootstrap';
+import CorrectSvg from '../../svgFiles/CorrectSvg';
+import { useNavigate, useParams } from 'react-router';
 
-
-import React from 'react'
-import './style.css'
-import Button from '../../components/shared/buttons/button'
-import tripSvgImage from '../../images/tripEnded.svg'
-import { Image } from 'react-bootstrap'
-import CorrectSvg from '../../svgFiles/CorrectSvg'
 const CompleteDelivery = () => {
-    return (
-        <div className='mobile_wrapper position-relative d-flex flex-column px-3 pt-3 gap-5 justify-content-center tripStarts'>
-            
-                <Image src={tripSvgImage} alt={'trip-picture'} className='trip-picture' />
-                
-        
-            <div className='picupForm '>
+    const navigate = useNavigate();
+    const { id, driverId } = useParams(); 
 
-                <h6 className='d-flex gap-2 align-items-center justify-content-center highlight'><CorrectSvg /> Trip Complete</h6>
-                <p className='text-center'>Thanks for completing your trip. You can upload any remaining documents within the next 48 hours.  Tap below to submit your delivery report.</p>
+    return (
+        <div className='mobile_wrapper position-relative d-flex flex-column px-3 pt-3 gap-4 justify-content-center tripStarts'>
+
+            <Image
+                src={tripSvgImage}
+                alt="trip-complete"
+                className="trip-picture"
+            />
+
+            <div className='picupForm text-center'>
+                <h6 className='d-flex gap-2 align-items-center justify-content-center highlight'>
+                    <CorrectSvg /> Trip Complete
+                </h6>
+
+                <p className='mt-2'>
+                    Thanks for completing your trip. You can upload any remaining
+                    documents within the next 48 hours.
+                </p>
+
+               
+                <Button
+                    label="Submit Delivery Report"
+                    className="rounded w-100 mt-3"
+                    onClick={() =>
+                        navigate(`/upload-documents/jobId/${id}/driver/${driverId}`)
+                    }
+                />
+
+             
+                <small className="text-muted d-block mt-2">
+                    You can also upload documents later from your trip history.
+                </small>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default CompleteDelivery
+export default CompleteDelivery;
