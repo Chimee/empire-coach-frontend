14850848b4bb7352b2fbc70e8d4baf76e74122d3
jyotishukla4ultivic <deepak.chauhan@ultivic.com>
2026-01-14 17:52:33 +0530

    changes in driver screen

--- src/features/driverScreens/rideEntry.jsx
+++ src/features/driverScreens/rideEntry.jsx
@@ -1,4 +1,5 @@
 import { useGetJobPickupDetailsQuery } from '../../app/driverApi/driverApi';
+import CompleteDelivery from './CompleteDelivery';
 import RideDetails from './rideDeatails'; // Fixed typo if applicable
 import RideStatusScreen from './RideStatusScreen';
 import UploadDocument from './UploadDocument';
@@ -6,19 +7,21 @@ import { useParams } from 'react-router';
 
 const RideEntry = () => {
     const { id } = useParams();
-    const { data: jobDetails, isLoading, isError } = 
+    const { data: jobDetails, isLoading, isError } =
         useGetJobPickupDetailsQuery({ id }, { skip: !id });
     const rideStatus = jobDetails?.data?.jobData?.request_status;
     if (isLoading) return <div>Loading ride details...</div>;
     if (isError || !rideStatus) return <div>Unable to load ride information.</div>;
     switch (rideStatus) {
         case "approved":
-        case "delivered":
             return <RideDetails />;
-        
+
         case "ride_started":
         case "in_transit":
             return <RideStatusScreen />;
+
+        case "delivered":
+            return <CompleteDelivery />
 
         default:
             return <div>Unknown status: {rideStatus}</div>;
